bf1b4e986ad49cfdcab719a295d139e7709f854d
Elena <elenreyes>
2025-11-05 19:57:11 +0100

    peek 4

--- peek.sh
+++ peek.sh
@@ -1,7 +1,10 @@
 
-if [[ -n "$2" ]];
-then head -n"$2" "$1" && echo ... && tail -n"$2" "$1";
-else [[ -z "$2" ]];
-head -n3 "$1" && echo ... && tail -n3 "$1";
+a=$(wc -l < $1);
+b=$(( 2 * $2 ));
+if [[ $b -ge "$a"  ]];
+then cat $1;
+else [[ "$a" -gt "$b" ]];
+	echo "WARNING! There are more than" $b "lines"	&&
+	head -n"$2" "$1" && echo ... && tail -n"$2" "$1";
 fi
 
