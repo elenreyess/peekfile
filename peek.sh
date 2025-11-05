14215d4cd2b4bb6225f9e935ca3473423d0a8090
Elena <elenreyes>
2025-11-05 19:24:36 +0100

    peek 3

--- peek.sh
+++ peek.sh
@@ -1,2 +1,7 @@
-head -n"$2" "$1" && echo ... && tail -n"$2" "$1"
+
+if [[ -n "$2" ]];
+then head -n"$2" "$1" && echo ... && tail -n"$2" "$1";
+else [[ -z "$2" ]];
+head -n3 "$1" && echo ... && tail -n3 "$1";
+fi
 
